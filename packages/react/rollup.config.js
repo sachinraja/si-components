f3cc59738b81b455f2dab84bc611075de1f2bb18
Sachin Raja <sachinraja2349@gmail.com>
2021-09-07 18:05:59 -0700

    fix: bundle react correctly (inject React)

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,9 +1,15 @@
 import createRollupConfig from '../../config/create-rollup-config'
 import esbuild from 'rollup-plugin-esbuild'
+import inject from '@rollup/plugin-inject'
 import dts from 'rollup-plugin-dts'
 
 export default createRollupConfig({
   input: 'index.tsx',
-  plugins: [esbuild()],
+  plugins: [
+    esbuild(),
+    inject({
+      React: 'react',
+    }),
+  ],
   dtsPlugins: [dts()],
 })
