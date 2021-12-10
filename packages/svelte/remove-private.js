9456822ed6af1d2ec68a14a4296832caaf8aeff6
Sachin Raja <sachinraja2349@gmail.com>
2021-12-09 23:32:18 -0800

    chore: update svelte dist dir in remove-private script

--- packages/svelte/remove-private.js
+++ packages/svelte/remove-private.js
@@ -2,7 +2,7 @@ import { readFileSync, writeFileSync } from 'fs'
 import path from 'path'
 
 // remove private: true from built package.json
-const distPackageJsonPath = path.join('package', 'package.json')
+const distPackageJsonPath = path.join('dist', 'package.json')
 const packageJson = JSON.parse(readFileSync(distPackageJsonPath))
 packageJson.private = undefined
 
